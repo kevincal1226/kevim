7d2c561ec382ac33c1d7a072347cb03a69dfb5df
Kevin Calopisis <kevincal@umich.edu>
2026-08-17 00:09:05 -0400

    change github texts

--- init.lua
+++ init.lua
@@ -368,11 +368,11 @@ do
   vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
   require('gitsigns').setup {
     signs = {
-      add = { text = '+' }, ---@diagnostic disable-line: missing-fields
-      change = { text = '~' }, ---@diagnostic disable-line: missing-fields
-      delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
-      topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
-      changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+      add = { text = '' }, ---@diagnostic disable-line: missing-fields
+      change = { text = '' }, ---@diagnostic disable-line: missing-fields
+      delete = { text = '' }, ---@diagnostic disable-line: missing-fields
+      topdelete = { text = '' }, ---@diagnostic disable-line: missing-fields
+      changedelete = { text = '' }, ---@diagnostic disable-line: missing-fields
     },
   }
 
